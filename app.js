9518d861520ace243488746c04a56735f467fe70
Naman Katewa <namankatewa2004@gmail.com>
2024-10-29 20:26:23 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,237 @@
+const locationSection = document.getElementById("location-data");
+const weatherSection = document.getElementById("weather-data");
+const newsSection = document.getElementById("news-data");
+
+// Function to change time every second
+function updateTime(timezone) {
+  const timePara = document.getElementById("localTime");
+  setInterval(() => {
+    const localTime = new Date().toLocaleString("en-US", {
+      timeZone: timezone,
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+    });
+    timePara.innerHTML = `<img src="icons/clock.svg">Local Time: ${localTime}`;
+  }, 1000);
+}
+
+// Fnction to map WMO weather code to their description
+function mapWeatherCodeToDescription(code) {
+  switch (code) {
+    case 0:
+      return "Clear Sky";
+    case 1:
+    case 2:
+      return "Partly Cloudy";
+    case 3:
+      return "Overcast";
+    case 45:
+    case 48:
+      return "Fog";
+    case 51:
+    case 53:
+    case 61:
+    case 63:
+    case 65:
+      return "Rain";
+    case 71:
+    case 73:
+    case 75:
+      return "Snow";
+    case 80:
+    case 81:
+    case 82:
+      return "Showers";
+    case 95:
+    case 96:
+    case 99:
+      return "Thunderstorm";
+    default:
+      return "Unknown Weather Condition";
+  }
+}
+
+// Function to get the weather icon based on the codition
+function getWeatherIcon(condition) {
+  switch (condition.toLowerCase()) {
+    case "clear sky":
+      return '<img class="weatherIcon" src="icons/sun.svg" />';
+    case "partly cloudy":
+      return '<img class="weatherIcon" src="icons/cloudy.svg" />';
+    case "fog":
+      return '<img class="weatherIcon" src="icons/cloudFog.svg" />';
+    case "rain":
+      return '<img class="weatherIcon" src="icons/cloudRain.svg" />';
+    case "snow":
+      return '<img class="weatherIcon" src="icons/cloudSnow.svg" />';
+    case "showers":
+      return '<img class="weatherIcon" src="icons/cloudRain.svg" />';
+    case "thunderstorm":
+      return '<img class="weatherIcon" src="icons/cloudLightning.svg" />';
+    default:
+      return '<img class="weatherIcon" src="icons/cloud.svg" />';
+  }
+}
+
+// Function to map Hourly Data
+function mapcurrentTimeToHourlyData(data) {
+  const currentTime = new Date(data.current_weather.time);
+  const roundedTime = new Date(
+    Date.UTC(
+      currentTime.getUTCFullYear(),
+      currentTime.getUTCMonth(),
+      currentTime.getUTCDate(),
+      currentTime.getUTCHours(),
+      currentTime.getUTCMinutes() >= 30 ? 60 : 0
+    )
+  );
+  const roundedHour = roundedTime.toISOString().slice(0, 16);
+  const index = data.hourly.time.findIndex((time) =>
+    time.startsWith(roundedHour)
+  );
+  return [
+    index !== -1 ? data.hourly.apparent_temperature[index] : null,
+    index !== -1 ? data.hourly.wind_speed_10m[index] : null,
+    index !== -1 ? data.hourly.relative_humidity_2m[index] : null,
+    index !== -1 ? data.hourly.surface_pressure[index] : null,
+    index !== -1 ? data.hourly.precipitation_probability[index] : null,
+    index !== -1 ? data.hourly.shortwave_radiation[index] : null,
+    index !== -1 ? data.hourly.direct_normal_irradiance[index] : null,
+    index !== -1 ? data.hourly.diffuse_radiation[index] : null,
+  ];
+}
+
+// Function to convert Wind Direction from Degrees to Readable Format
+function getWindDirection(degrees) {
+  const directions = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"];
+  const index = Math.round(degrees / 45) % 8;
+  return directions[index];
+}
+
+// Function to calculate UV Index from Solar Radiation
+function calculateUVIndex(data) {
+  const ghi = data[4];
+  const dni = data[5];
+  const dhi = data[6];
+  const tsi = ghi + dni + dhi;
+  const uvIndex = tsi * 0.005;
+  return uvIndex.toFixed(2);
+}
+
+// Function to convert into readable time
+function simpleTime(isoString) {
+  const date = new Date(isoString);
+  let hours = date.getHours();
+  const minutes = date.getMinutes();
+  const ampm = hours >= 12 ? "PM" : "AM";
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+  const minutesStr = minutes < 10 ? "0" + minutes : minutes;
+  return `${hours}:${minutesStr} ${ampm}`;
+}
+
+// Wait for the DOM content to load before doing anything
+document.addEventListener("DOMContentLoaded", function () {
+  // Fetch the location from the IP address
+  fetch("https://ipapi.co/json")
+    .then((response) => response.json())
+    .then((data) => {
+      // Store details in the locationData variable
+      const locationData = data;
+
+      // Show location data in the DOM
+      locationSection.innerHTML = `<p>
+      <img src="icons/mapPin.svg"> 
+      ${locationData.city}, ${locationData.country_name}
+      </p>
+      <p>
+      <img src="icons/compass.svg"> 
+      Lat: ${locationData.latitude}, Long: ${locationData.longitude}
+      </p>
+      <p id="localTime" >
+      <img src="icons/clock.svg">
+      Local Time: 
+      </p>
+      <p>
+      <img src="icons/calendar.svg"> 
+      Timezone: ${locationData.timezone}
+      </p>
+      <p id="sunrise"><img src="icons/sunrise.svg"></p>
+      <p id="sunset"><img src="icons/sunset.svg"></p>`;
+
+      // Update the clock Realtime
+      updateTime(locationData.timezone);
+
+      // Fetch Weather Details from Open Meteo
+      fetch(
+        `https://api.open-meteo.com/v1/forecast?latitude=${locationData.latitude}&longitude=${locationData.longitude}&current_weather=true&hourly=apparent_temperature,wind_speed_10m,relative_humidity_2m,surface_pressure,precipitation_probability,shortwave_radiation,direct_normal_irradiance,diffuse_radiation&daily=sunrise,sunset
+`
+      )
+        .then((res) => res.json())
+        .then((weather) => {
+          sunrise = document.getElementById("sunrise");
+          sunset = document.getElementById("sunset");
+          sunrise.innerHTML += `Sunrise: ${simpleTime(
+            weather.daily.sunrise[0]
+          )}`;
+          sunset.innerHTML += `Sunset: ${simpleTime(weather.daily.sunset[0])}`;
+          weatherSection.innerHTML = `${getWeatherIcon(
+            mapWeatherCodeToDescription(weather.current_weather.weathercode)
+          )}
+          <p class="temperature">${weather.current_weather.temperature}${
+            weather.current_weather_units.temperature
+          }</p>
+          <p class="condition">${mapWeatherCodeToDescription(
+            weather.current_weather.weathercode
+          )}</p>
+                      <div class="weather-details">
+                <p>
+                    <img src="icons/thermometer.svg" />
+                    Feels like: ${mapcurrentTimeToHourlyData(weather)[0]}${
+            weather.hourly_units.apparent_temperature
+          }
+                </p>
+                <p>
+                    <img src="icons/droplets.svg" />
+                    Humidity: ${mapcurrentTimeToHourlyData(weather)[2]}${
+            weather.hourly_units.relative_humidity_2m
+          }
+                </p>
+                <p>
+                    <img src="icons/wind.svg" />
+                    Wind: ${mapcurrentTimeToHourlyData(weather)[1]} ${
+            weather.hourly_units.wind_speed_10m
+          } ${getWindDirection(weather.current_weather.winddirection)}
+                </p>
+                <p>
+                    <img src="icons/compass.svg" />
+                    Pressure: ${mapcurrentTimeToHourlyData(weather)[3]}${
+            weather.hourly_units.surface_pressure
+          }
+                </p>
+                <p>
+                    <img src="icons/sun.svg" />
+                    UV Index: ${calculateUVIndex(
+                      mapcurrentTimeToHourlyData(weather)
+                    )}
+                </p>
+                <p>
+                    <img src="icons/cloudRain.svg" />
+                    Precipitation: ${mapcurrentTimeToHourlyData(weather)[4]}${
+            weather.hourly_units.precipitation_probability
+          }
+                </p>
+            </div>
+          `;
+        })
+        // Handling errors when fetching Weather
+        .catch((err) => {
+          console.error("Error fetching weather", err);
+        });
+    })
+    // Handling errors when fetching Location
+    .catch((error) => {
+      console.error("Error fetching location", error);
+    });
+});
